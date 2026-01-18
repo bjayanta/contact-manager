e0dbc78dab7bf41058215bd0c2aa0c670ad2289f
Jayanta <bjayanta.me@gmail.com>
2026-01-18 22:36:46 +0600

    practice controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Query,
   Redirect,
   Req,
+  Res,
 } from '@nestjs/common';
 import type { Request } from 'express';
 
@@ -17,7 +18,6 @@ export class UsersController {
   // findAll(): string {
   //   return 'This is the list of all users';
   // }
-
   // @Get()
   // findAll(@Req() request: Request): string {
   //   // console.log('Request: ', request);
@@ -26,43 +26,42 @@ export class UsersController {
   //   console.log('Host name: ', request.headers.host);
   //   return 'This is the list of all users';
   // }
-
   // @Get('abc/*')
   // findAll(): string {
   //   return 'This is the list of all users';
   // }
-
   // @Get()
   // @Redirect('https://nestjs.com', 301)
   // findAll(): string {
   //   return 'This is the list of all users';
   // }
-
-  @Get()
-  @Redirect('https://nestjs.com', 301)
-  findAll(@Query('version') version): any {
-    if (version && version === '5') {
-      return {
-        url: 'https://docs.nestjs.com/v5/',
-      };
-    }
-  }
-
-  @Post()
-  // @HttpCode(204)
-  @Header('Cache-Control', 'no-store')
-  create(): string {
-    return 'An user created successfully.';
-  }
-
+  // @Get()
+  // @Redirect('https://nestjs.com', 301)
+  // findAll(@Query('version') version): any {
+  //   if (version && version === '5') {
+  //     return {
+  //       url: 'https://docs.nestjs.com/v5/',
+  //     };
+  //   }
+  // }
+  // @Post()
+  // // @HttpCode(204)
+  // @Header('Cache-Control', 'no-store')
+  // create(): string {
+  //   return 'An user created successfully.';
+  // }
   // @Get(':id')
   // findOne(@Param() params: { id: string }): string {
   //   console.log(params);
   //   return `This action returns a ${params.id} user`;
   // }
+  // @Get(':id')
+  // findOne(@Param('id') id: string): string {
+  //   return `This action returns a ${id} user`;
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string): string {
-    return `This action returns a ${id} user`;
+  @Get()
+  async findAll(): Promise<any[]> {
+    return (await this.findAll()) || [];
   }
 }
