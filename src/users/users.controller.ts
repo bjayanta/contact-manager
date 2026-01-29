c8d4103dc637adadf2f7ef0d3f36b41fc59f8291
Jayanta Biswas <jayanta.biswas@lyxa.ai>
2026-01-29 12:26:50 +0600

    start users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,67 +1,34 @@
-import {
-  Controller,
-  Get,
-  Header,
-  HttpCode,
-  Param,
-  Post,
-  Query,
-  Redirect,
-  Req,
-  Res,
-} from '@nestjs/common';
-import type { Request } from 'express';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  // @Get()
-  // findAll(): string {
-  //   return 'This is the list of all users';
-  // }
-  // @Get()
-  // findAll(@Req() request: Request): string {
-  //   // console.log('Request: ', request);
-  //   // console.log('IP: ', request.ip);
-  //   // console.log('Params: ', request.params);
-  //   console.log('Host name: ', request.headers.host);
-  //   return 'This is the list of all users';
-  // }
-  // @Get('abc/*')
-  // findAll(): string {
-  //   return 'This is the list of all users';
-  // }
-  // @Get()
-  // @Redirect('https://nestjs.com', 301)
-  // findAll(): string {
-  //   return 'This is the list of all users';
-  // }
-  // @Get()
-  // @Redirect('https://nestjs.com', 301)
-  // findAll(@Query('version') version): any {
-  //   if (version && version === '5') {
-  //     return {
-  //       url: 'https://docs.nestjs.com/v5/',
-  //     };
-  //   }
-  // }
-  // @Post()
-  // // @HttpCode(204)
-  // @Header('Cache-Control', 'no-store')
-  // create(): string {
-  //   return 'An user created successfully.';
-  // }
-  // @Get(':id')
-  // findOne(@Param() params: { id: string }): string {
-  //   console.log(params);
-  //   return `This action returns a ${params.id} user`;
-  // }
-  // @Get(':id')
-  // findOne(@Param('id') id: string): string {
-  //   return `This action returns a ${id} user`;
-  // }
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
 
   @Get()
-  async findAll(): Promise<any[]> {
-    return (await this.findAll()) || [];
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
   }
 }
